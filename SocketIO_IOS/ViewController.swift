9d3d6845d8af367a73aeb281571053e5e1503a3e
Shashank Gupta <shashankgupta@Shashanks-MacBook-Pro.local>
2016-06-03 23:06:44 +0530

    Socket Implementation

---
+++ SocketIO_IOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SocketIO_IOS
+//
+//  Created by Shashank Gupta on 03/06/16.
+//  Copyright © 2016 Shashank Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
+
